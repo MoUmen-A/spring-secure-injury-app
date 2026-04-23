fbff088ffddcacd074f1a428f52f1156922c62af
MoUmen-A <A.Ahmed64732@student.aast.edu>
2026-04-23 16:11:32 +0200

    model scaffold: keep BodyPart as domain placeholder for team implementation

--- src/main/java/dev/mr3/sb/model/BodyPart.java
+++ src/main/java/dev/mr3/sb/model/BodyPart.java
@@ -1,18 +1,4 @@
 package dev.mr3.sb.model;
 
 public enum BodyPart {
-    THIGH,
-    HAMSTRING,
-    CALF,
-    ANKLE,
-    KNEE,
-    FOOT,
-    SHIN,
-    ARM,
-    LEG,
-    WRIST,
-    SHOULDER,
-    ELBOW,
-    ACHILLES;
-
 }
