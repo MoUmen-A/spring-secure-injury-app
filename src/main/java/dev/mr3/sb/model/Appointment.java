eb200e2c07825d9f2c80e0cd6fbac91a6e5ab762
MoUmen-A <A.Ahmed64732@student.aast.edu>
2026-04-23 22:15:27 +0200

    Revert "model scaffold: keep Appointment as domain placeholder for team implementation"
    This reverts commit 09e972c35501abc0fabf4034e29e86241051ec53.

--- src/main/java/dev/mr3/sb/model/Appointment.java
+++ src/main/java/dev/mr3/sb/model/Appointment.java
@@ -1,4 +1,42 @@
 package dev.mr3.sb.model;
 
+import jakarta.persistence.*;
+
+
+@Entity
 public class Appointment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private  Weekday weekday;
+    @Column
+    private String time;
+
+
+    public Appointment() {
+    }
+    public Appointment(Weekday weekday, String time) {
+        this.weekday = weekday;
+        this.time = time;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Weekday getWeekday() {
+        return weekday;
+    }
+    public void setWeekday(Weekday weekday) {
+        this.weekday = weekday;
+    }
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
