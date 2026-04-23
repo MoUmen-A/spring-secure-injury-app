dddbc9814d04433176b8e63ce666a0b2a2789d13
MoUmen-A <A.Ahmed64732@student.aast.edu>
2026-04-23 16:37:37 +0200

    refactor: expand BodyPart enum with additional body parts

--- src/main/java/dev/mr3/sb/model/BodyPart.java
+++ src/main/java/dev/mr3/sb/model/BodyPart.java
@@ -1,4 +1,18 @@
 package dev.mr3.sb.model;
 
 public enum BodyPart {
+    THIGH,
+    HAMSTRING,
+    CALF,
+    ANKLE,
+    KNEE,
+    FOOT,
+    SHIN,
+    ARM,
+    LEG,
+    WRIST,
+    SHOULDER,
+    ELBOW,
+    ACHILLES;
+
 }
